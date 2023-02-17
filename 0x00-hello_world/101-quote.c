ed00fc9ba53378cd7b34fe7df15970053e68c1de
Peter <you@example.com>
2023-02-17 02:47:20 -0800

    UNIX is basically a simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *  main - UNIX is basically a simple operating system
+ *  Return: 1
+ */
+int main(void)
+{
+fput("that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n", stdout);
+return (1)
+}
